caab2dc68f2f93d085851e352c841c72a62521e0
Rafael Costa da Silva <fael.rgt@gmail.com>
2020-02-16 20:06:31 -0300

    put data edit and delete

--- modulo1/index.js
+++ modulo1/index.js
@@ -1,9 +1,43 @@
-const express = require(`express`);
+const express = require('express');
 
 const server = express();
 
-server.get(`/teste`, (req, res) => {
-  return res.json({ message: 'Hello World' });
+server.use(express.json());
+
+const users = ['Rafael', 'Filipe', 'David'];
+
+server.get('/users', (req, res) => {
+
+  return res.json(users);
 })
+
+server.get(`/users/:index`, (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+})
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+  return res.send();
+});
 
 server.listen(3000);
